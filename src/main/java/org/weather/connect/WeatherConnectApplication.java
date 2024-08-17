6288d4f4b44908d623d9992243c99e0597ee0f23
Suhas-C-V <cvsuhas.299@gmail.com>
2024-08-17 17:34:46 +0530

    Add Spring boot main method and run methos.

---
+++ src/main/java/org/weather/connect/WeatherConnectApplication.java
@@ -0,0 +1,14 @@
+package org.weather.connect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class WeatherConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherConnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/weather/connect/WeatherConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.weather.connect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
